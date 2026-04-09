19623c11fec4f70070b6af849bc83835017e8b55
Parth valia <parthvalia2709@gmail.com>
2026-04-10 01:03:01 +0530

    feat: implement integrity and attestation features with Dok API

--- src/components/MainApp.js
+++ src/components/MainApp.js
@@ -1,4 +1,4 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useEffect} from 'react';
 import {Provider} from 'react-redux';
 import {store} from 'redux/store';
 import {Provider as PaperProvider} from 'react-native-paper';
@@ -8,11 +8,21 @@ import Toasts from 'components/Toasts';
 import ErrorBoundary from 'react-native-error-boundary';
 import ErrorComponent from 'components/ErrorComponent';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {DokApi} from 'dok-wallet-blockchain-networks/config/dokApi';
+import {
+  initializeDokApiIntegrity,
+  setupDokApiIntegrity,
+} from 'utils/apiIntegrity';
 
 export default function MainApp() {
   const onError = useCallback((error, stackTrace) => {
     console.error('Error in app', error.message);
     console.error('Error in app stacktrace', stackTrace);
+  }, []);
+
+  useEffect(() => {
+    setupDokApiIntegrity(DokApi);
+    initializeDokApiIntegrity();
   }, []);
 
   return (
