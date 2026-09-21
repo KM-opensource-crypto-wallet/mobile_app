1771395b1b5d1d9a8a747c1f60fee58a5d1fe34c
Divyang Khatri <divyangkhatri.dk@gmail.com>
2026-09-21 16:27:40 +0530

    remove log

--- src/screens/main/Home/HomeScreen/index.js
+++ src/screens/main/Home/HomeScreen/index.js
@@ -156,8 +156,6 @@ const HomeScreen = ({navigation, route}) => {
   const wcUri = useSelector(getWCUri);
   const routeStateData = useSelector(getRouteStateData);
   const conversationName = useSelector(getConversationName);
-  const allState = useSelector(state => state);
-  console.log('allState', allState);
 
   const newsBottomSheetRef = useRef();
   const selectedTopicRef = useRef();
